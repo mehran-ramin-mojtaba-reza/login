d43f76f332595003cbf66d4aa6d99cb068b4153f
ali <ali82@gmail.com>
2023-06-06 11:04:22 +0430

    hesam first commit

---
+++ app/src/main/java/com/example/mhhgtcfv5555/Destinations.kt
@@ -0,0 +1,11 @@
+package com.example.mhhgtcfv5555
+
+interface Destinations {
+    val Route:String
+}
+object Home:Destinations{
+    override val Route = "Home"
+}
+object Second:Destinations{
+    override val Route = "Second"
+}
